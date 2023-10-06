5c873a394f9db2d8a6c661f91a8efc43b9131e31
adesug <adesugiantoro89@gmail.com>
2023-10-06 20:33:24 +0700

    add active sidebar

--- resources/views/layouts/pages/sidebar.blade.php
+++ resources/views/layouts/pages/sidebar.blade.php
@@ -15,7 +15,7 @@
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
             <li class="nav-item">
-                <a href="{{route('superAdmin.home')}}" class="nav-link {{(request()->is('admin/home')) ? 'active' : ''}}">
+                <a href="{{route('superAdmin.home')}}" class="nav-link {{(request()->is('admin/home') || (request()->is('home'))) ? 'active' : ''}}">
                     <i class="nav-icon fas fa-tachometer-alt"></i>
                   <p>
                     Home
